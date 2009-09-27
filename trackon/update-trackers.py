2f80942db93a86127f2ec4cf8f263607734f47ba
Uriel <uriel@berlinblue.org>
2009-09-27 03:12:04 +0200

    New check scheduling algorithm that is more flexible and takes into account min-interval.

--- trackon/update-trackers.py
+++ trackon/update-trackers.py
@@ -1,27 +1,41 @@
 from logging import debug, error, info
-from cgi import parse_qs, FieldStorage
+from cgi import FieldStorage
 from time import time
 from google.appengine.api.labs import taskqueue as tq
-from google.appengine.api import memcache as mc
 from trackon import tracker
 
 update_queue = tq.Queue('update-trackers')
+MAX_MIN_INTERAVAL = 60*60*10
+DEFAULT_CHECK_INTERVAL = 60*15
+
 
 def main():
     args = FieldStorage()
+    now = int(time())
 
     if 'tracker-address' in args:
         t = args['tracker-address'].value
         (r, url) = tracker.check(t)
+
+        nxt = DEFAULT_CHECK_INTERVAL
+        if 'response' in r and 'min interval' in r['response']:
+            nxt = r['response']['min interval']
+
+            if nxt > MAX_MIN_INTERVAL:
+                nxt = MAX_MIN_INTERVAL
+                
+        r['next-check'] = now+nxt
         tracker.update(t, r)
         if 'error' in r:
             info("Update failed for %s: %s" % (t, r['error']))
         
     else:
-        for t in (mc.get('tracker-list') or []):
-            params = {'tracker-address': t}
-            task = tq.Task(params=params) # TODO set countdown to min interval
-            update_queue.add(task)
+        ti = tracker.allinfo() or {}
+        for t in ti:
+            if 'next-check' not in ti[t] or ti[t]['next-check'] < now: 
+                params = {'tracker-address': t}
+                task = tq.Task(params=params)
+                update_queue.add(task)
         
 
 if __name__ == '__main__':
