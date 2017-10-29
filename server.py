743de658638ccc2af889c79c4cac9f0eaea4961e
CorralPeltzer <corral.miguelangel@gmail.com>
2017-10-30 00:50:05 +0100

    Change from Bottle to Flask framework

--- server.py
+++ server.py
@@ -1,20 +1,39 @@
 import logging
 import threading
 from logging import FileHandler
-from bottle import Bottle, run, static_file, request, response, HTTPError, redirect, mako_template as template
+
+from flask import Flask, send_from_directory, request, Response, redirect, make_response, abort
+from flask_mako import MakoTemplates, render_template
+from werkzeug.routing import BaseConverter
 from requestlogger import WSGILogger, ApacheFormatter
+from cheroot.wsgi import Server
+
 import trackon
 import trackerlist_project
 
-app = Bottle()
+mako = MakoTemplates()
+app = Flask(__name__)
+app.template_folder = "tpl"
+mako.init_app(app)
 
+
+accepted_hosts = {'localhost:8080', 'localhost', '127.0.0.1', '127.0.0.1:8080'}
+canonical_url = 'http://localhost:8080/'
+
+
+class RegexConverter(BaseConverter):
+    def __init__(self, url_map, *items):
+        super(RegexConverter, self).__init__(url_map)
+        self.regex = items[0]
+
+
+app.url_map.converters['regex'] = RegexConverter
 logger = logging.getLogger('trackon_logger')
 logger.setLevel(logging.INFO)
 handler = logging.FileHandler('trackon.log')
 logger_format = logging.Formatter('%(asctime)s - %(message)s')
 handler.setFormatter(logger_format)
 logger.addHandler(handler)
-
 logger.info('Server started')
 
 
@@ -22,64 +41,63 @@ logger.info('Server started')
 def main():
     trackers_list = trackon.get_all_data_from_db()
     trackers_list = trackon.process_uptime_and_downtime_time(trackers_list)
-    return template('tpl/main.mako', trackers=trackers_list, active='main')
+    return render_template('main.mako', trackers=trackers_list, active='main')
 
 
-@app.route('/', method='POST')
+@app.route('/', methods=['POST'])
 def new_trackers():
-    new_ts = request.forms.get('new_trackers')
+    new_ts = request.form.get('new_trackers')
     check_all_trackers = threading.Thread(target=trackon.enqueue_new_trackers, args=(new_ts,))
     check_all_trackers.daemon = True
     check_all_trackers.start()
     return main()
 
 
-@app.route('/api/add', method='POST')
+@app.route('/api/add', methods=['POST'])
 def new_trackers_api():
-    response.set_header("Access-Control-Allow-Origin", "*")
-    new_ts = request.forms.get('new_trackers')
+    new_ts = request.form.get('new_trackers')
     check_all_trackers = threading.Thread(target=trackon.enqueue_new_trackers, args=(new_ts,))
     check_all_trackers.daemon = True
     check_all_trackers.start()
-    response.status = 204
-    return response
+    resp = Response(status=204, headers={'Access-Control-Allow-Origin': '*'})
+    return resp
 
 
 @app.route('/submitted')
 def submitted():
-    return template('tpl/submitted.mako', data=trackon.submitted_data, size=len(trackon.submitted_trackers), active='submitted')
+    return render_template('submitted.mako', data=trackon.submitted_data, size=len(trackon.submitted_trackers), active='submitted')
 
 
 @app.route('/faq')
 def faq():
-    return template('tpl/static/faq.mako', active='faq')
+    return render_template('/static/faq.mako', active='faq')
 
 
 @app.route('/list')
 def list_stable():
     stable_list, size = trackon.list_uptime(95)
-    return template('tpl/list.mako', stable=stable_list, size=size, active='list')
+    return render_template('list.mako', stable=stable_list, size=size, active='list')
 
 
 @app.route('/api')
 def api():
-    return template('tpl/static/api-docs.mako', active='api')
+    return render_template('/static/api-docs.mako', active='api')
 
 
 @app.route('/raw')
 def raw():
-    return template('tpl/raw.mako', data=trackon.raw_data, active='raw')
+    return render_template('raw.mako', data=trackon.raw_data, active='raw')
 
 
-@app.route('/api/<percentage:int>')
+@app.route('/api/<int:percentage>')
 def api_percentage(percentage):
     if 0 <= percentage <= 100:
-        add_api_headers()
         formatted_list, not_needed_length = trackon.list_uptime(percentage)
-        return formatted_list
+        resp = make_response(formatted_list)
+        resp = add_api_headers(resp)
+        return resp
     else:
-        # abort(400, "The percentage has to be between 0 an 100") Abort does not allow custom headers
-        raise HTTPError(status=400, body="The percentage has to be between 0 an 100", headers={"Access-Control-Allow-Origin": "*"})
+        abort(Response("The percentage has to be between 0 an 100", 400, headers={'Access-Control-Allow-Origin': '*'}))
 
 
 @app.route('/api/stable')
@@ -94,53 +112,51 @@ def api_all():
 
 @app.route('/api/live')
 def api_live():
-    add_api_headers()
-    return trackon.list_live()
+    resp = make_response(trackon.list_live())
+    resp = add_api_headers(resp)
+    return resp
 
 
 @app.route('/api/udp')
 def api_udp():
-    add_api_headers()
-    return trackon.api_udp()
+    resp = make_response(trackon.api_udp())
+    resp = add_api_headers(resp)
+    return resp
 
 
 @app.route('/api/http')
 def api_http():
-    add_api_headers()
-    return trackon.api_http()
+    resp = make_response(trackon.api_http())
+    resp = add_api_headers(resp)
+    return resp
 
 
 @app.route('/about')
 def about():
-    return template('tpl/static/about.mako', active='about')
+    return render_template('/static/about.mako', active='about')
 
 
-@app.route('/static/<path:path>')  # matches any static file
-def static(path):
-    return static_file(path, root='static')
-
-
-@app.route('/<filename>.<filetype:re:(png|svg|ico)>')  # matches all favicons that should be in root
+@app.route('/<regex(".*(?=\.)"):filename>.<regex("(png|svg|ico)"):filetype>')  # matches all favicons that should be in root
 def favicon(filename, filetype):
-    response.content_type = 'image/x-icon'
-    return static_file(filename + '.' + filetype, root='static/imgs')
+    return send_from_directory('static/imgs/', filename + '.' + filetype)
 
 
-@app.route('/<filename>.<filetype:re:(xml|json)>')  # matches browserconfig and manifest that should be in root
+@app.route('/<regex(".*(?=\.)"):filename>.<regex("(xml|json)"):filetype>') # matches browserconfig and manifest that should be in root
 def app_things(filename, filetype):
-    return static_file(filename + '.' + filetype, root='static')
+    return send_from_directory('static/', filename + '.' + filetype)
 
 
-@app.hook('after_request')
+@app.before_request
 def check_host_http_header():
-    accepted_hosts = {'localhost:8080', 'localhost', '127.0.0.1:8080', '127.0.0.1'}
-    if request.headers['host'] not in accepted_hosts:
-        redirect('http://localhost:8080/', 301)
+    if request.headers.get('host') not in accepted_hosts:
+        return redirect(canonical_url, code=301)
 
 
-def add_api_headers():
-    response.set_header("Access-Control-Allow-Origin", "*")
-    response.content_type = 'text/plain'
+def add_api_headers(resp):
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.mimetype = 'text/plain'
+    return resp
+
 
 update_status = threading.Thread(target=trackon.update_outdated_trackers)
 update_status.daemon = True
@@ -153,5 +169,10 @@ get_trackerlist_project_list.start()
 handlers = [FileHandler('access.log'), ]
 app = WSGILogger(app, handlers, ApacheFormatter())
 
+server = Server(('0.0.0.0', 8080), app)
+
 if __name__ == '__main__':
-    run(app, host='localhost', port=8080, server='waitress')
+    try:
+        server.start()
+    except KeyboardInterrupt:
+        server.stop()
