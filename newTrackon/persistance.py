4a86d11ea50bda8dafbcea4e843368a6bb4af9ab
CorralPeltzer <corral.miguelangel@gmail.com>
2020-02-09 16:31:07 +0100

    Save activity history in json instead of pickle

--- newTrackon/persistance.py
+++ newTrackon/persistance.py
@@ -1,21 +1,21 @@
 from os import path
 from collections import deque
-import pickle
+import json
 
 submitted_trackers = deque(maxlen=10000)
-raw_history_file = "data/raw_data.pickle"
-submitted_history_file = "data/submitted_data.pickle"
+raw_history_file = "data/raw_data.json"
+submitted_history_file = "data/submitted_data.json"
 
 
 if path.exists(raw_history_file):
-    raw_data = pickle.load(open(raw_history_file, "rb"))
+    raw_data = deque(json.load(open(raw_history_file, "r")), maxlen=600)
 else:
     raw_data = deque(maxlen=600)
 if path.exists(submitted_history_file):
-    submitted_data = pickle.load(open(submitted_history_file, "rb"))
+    submitted_data = deque(json.load(open(submitted_history_file, "r")), maxlen=600)
 else:
     submitted_data = deque(maxlen=600)
 
 
-def save_obj_to_disk(obj, filename):
-    pickle.dump(obj, open(filename, "wb"))
+def save_deque_to_disk(obj, filename):
+    json.dump(list(obj), open(filename, "w"))
